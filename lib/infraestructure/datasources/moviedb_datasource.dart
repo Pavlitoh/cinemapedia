24736a69223cc0bab56852eb25a0608296c484fc
Pablo Gonzalez <hjuan247@gmail.com>
2024-12-13 12:03:50 -0600

    cambios en moviedb datasource, mapper para vinculacion con mi entidad

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
-import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
-import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:dio/dio.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/moviedb_response.dart';
 
 class MoviedbDatasource extends MoviesDatasource{
   final dio = Dio(BaseOptions(baseUrl: 'https://api.themoviedb.org/3',
@@ -13,11 +15,19 @@ class MoviedbDatasource extends MoviesDatasource{
   @override
   Future<List<Movie>> getNowPLaying({int page = 1}) async {
 
-    final reponse = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final response = await dio.get('/movie/now_playing');
+    
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+    
+    final List<Movie> movies = movieDbResponse.results
+    //si el poster no existe, no se va a renderizar la pelicula
+    .where((moviedb) => moviedb.posterPath != 'no-poster')
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb))
+    .toList();
 
 
-    return [];
+    return movies;
   }
 
 }
